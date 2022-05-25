fad04300bf5626a3f7907e7b2b456c33f347edc6
ZeyarMin <zeyermin@gmail.com>
2022-05-25 21:51:09 +0630

    May25Night

--- src/views/menus/sub-menu-1/result.js
+++ src/views/menus/sub-menu-1/result.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
-import './style.css'
+import "./style.css";
 function Add(props) {
-    return(
-        <div>
-            <input type={'text'} value={props.firstInput} onChange={props.firstChange} />
-            <label value={props.secInput} onChange={props.secChange} className={"opsign"}>+</label>
-            <input type={'text'} value={props.thirdInput} onChange={props.thirdChange} />
-            <button type="button" onClick={props.equalto}>=</button>
-            <input type={'text'} value={props.resultInput}/>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        type={"text"}
+        className="inputCalc"
+        value={props.firstInput}
+        onChange={props.firstChange}
+      />
+      <label
+        value={props.secInput}
+        onChange={props.secChange}
+        className={"opsign"}
+      >
+        +
+      </label>
+      <input
+        type={"text"}
+        className="inputCalc"
+        value={props.thirdInput}
+        onChange={props.thirdChange}
+      />
+      <button type="button" className="btnCalc" onClick={props.equalto}>
+        =
+      </button>
+      <input type={"text"} className="inputCalc" value={props.resultInput} />
+    </div>
+  );
 }
 
 export default Add;
--- src/views/menus/sub-menu-1/subs.js
+++ src/views/menus/sub-menu-1/subs.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 
 const Subs = (props) => {
-    return (
-        <div className="Subs">
-            <input type={'text'} value={props.subsFirst} onChange={props.subfirstChange} />
-            <label onChange={props.secChange} className={"opsign"}>-</label>
-            <input type={'text'} value={props.substhird} onChange={props.subthirdChange} />
-            <button type="button" onClick={props.subisequalto}>=</button>
-            <input type={'text'} value={props.subsResult}/>
-        </div>
-    )
-}
+  return (
+    <div className="Subs">
+      <input
+        className="inputCalc"
+        type={"text"}
+        value={props.subsFirst}
+        onChange={props.subfirstChange}
+      />
+      <label
+        className="inputCalc"
+        onChange={props.secChange}
+        className={"opsign"}
+      >
+        -
+      </label>
+      <input
+        className="inputCalc"
+        type={"text"}
+        value={props.substhird}
+        onChange={props.subthirdChange}
+      />
+      <button className="btnCalc" type="button" onClick={props.subisequalto}>
+        =
+      </button>
+      <input className="inputCalc" type={"text"} value={props.subsResult} />
+    </div>
+  );
+};
 
 export default Subs;
